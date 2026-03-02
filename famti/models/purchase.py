b163dc46edf56ae3eb16a177d9f08026d39e7340
Kritika <Kritika.Verma@ageistech.com>
2026-03-02 20:00:53 +0530

    Reupdate code without conflict

--- famti/models/purchase.py
+++ famti/models/purchase.py
@@ -25,6 +25,7 @@ class Purchase(models.Model):
     vendor_email = fields.Char(string="Email")
     vendor_phone = fields.Char(string="Contact")
     is_tolling = fields.Boolean(string='Is Tolling?')
+    logo = fields.Image("Logo", max_width=1920, max_height=1920, default=lambda self: self.env.company.logo)
 
     remarks = fields.Text("Remarks")
 
